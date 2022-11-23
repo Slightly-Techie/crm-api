8eed47786b783b9a5b999fccc224b7ca3acf288d
RansfordGenesis <jefferykyeigenesis@gmail.com>
2022-11-23 21:09:35 +0000

    signup

--- app.py
+++ app.py
@@ -1,7 +1,35 @@
 from fastapi import FastAPI
 
+from routes.auth import auth_router
+from database import engine
+
+import models
+
+models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return "Hello Slightly Techie"
+
+@app.get('/')
+def index():
+  return {"msg":"home"}
+
+app.include_router(auth_router, prefix="/api/v1")
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  # pip cache purge
+  # pip config set global.no-cache-dir false
---
+++ schema.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, EmailStr
+
+class UserSignUp(BaseModel):
+  email: EmailStr
+  first_name: str
+  last_name: str
+  password: str
+  password_confirmation: str
+
+class UserResponse(BaseModel):
+  id: int
+  email: EmailStr
+  first_name: str
+  last_name: str
