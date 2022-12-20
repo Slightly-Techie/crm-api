81483ee76fbc772d3decb370416ff7d94a538b90
JerryAgbesi <jerryeagbesi@gmail.com>
2022-12-20 13:04:10 +0000

    Refactor profile update

--- api/routes/profile_page.py
+++ api/routes/profile_page.py
@@ -17,25 +17,20 @@ async def get_profile(id:int,db: Session = Depends(get_db)) -> UserResponse:
 
 @profile_route.put("/profile")
 async def update_profile(id:int,userDetails:Profile,db:Session = Depends(get_db)):
-    user_exists =  db.query(User).filter(User.id == id).first()
+    user_exists =  db.query(User).filter(User.id == id)
     try:
-        if user_exists:
-            user_exists.email = userDetails.email if userDetails.email else user_exists.email
-            user_exists.first_name = userDetails.first_name if userDetails.first_name else user_exists.first_name
-            user_exists.last_name = userDetails.last_name if userDetails.last_name else user_exists.last_name
-            user_exists.github_profile = userDetails.github_profile
-            user_exists.twitter_profile = userDetails.twitter_profile
-            user_exists.linkedin_profile = userDetails.linkedin_profile
-            user_exists.portfolio_url = userDetails.portfolio_url
-            user_exists.profile_pic_url = userDetails.profile_pic_url
-
+        if user_exists.first():
+            update_data = userDetails.dict(exclude_unset=True) #convert the json into a python dict and exclude fields not specified
+            user_exists.update(update_data)
+                        
             db.commit()
 
             return {"message" : "Profile update successful"}
-        
+                    
         else:
-            raise HTTPException(status_code=404,detail="Id does not exist")  
+                raise HTTPException(status_code=404,detail="Id does not exist")                       
     except:
         raise HTTPException(status_code=400,detail="Something went wrong")
+        
 
 
