8b0b480daef9d9b17fb0c81787b9c3799569235a
eeTiboah <etiboah@gmail.com>
2022-12-21 17:13:24 +0000

    add an else condition

--- test/database.py
+++ test/database.py
@@ -15,8 +15,8 @@ POSTGRES_DB="github_actions_db"
 
 if os.getenv('CI'):
     TEST_SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@localhost:{POSTGRES_PORT}/{POSTGRES_DB}"
-
-TEST_SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_SERVER}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}_test"
+else:
+    TEST_SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_SERVER}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}_test"
 
 engine = create_engine(TEST_SQLALCHEMY_DATABASE_URL)
 
