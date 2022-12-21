5187c9f61d96b5c9c2b37539855048bef40579ce
eeTiboah <etiboah@gmail.com>
2022-12-21 21:51:32 +0000

    remove dotenvpath argument from load_dotenv

--- core/config.py
+++ core/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 from pathlib import Path
 env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path)
+load_dotenv()
 
 class Settings:
     PROJECT_NAME:str = "Job Board"
