ff3d9d1540598b838db9f33b01bbfe5d1821c4c8
eeTiboah <etiboah@gmail.com>
2022-12-21 17:11:58 +0000

    edit the os environ

--- test/database.py
+++ test/database.py
@@ -13,7 +13,7 @@ POSTGRES_PASSWORD="postgres"
 POSTGRES_PORT = "5432"
 POSTGRES_DB="github_actions_db"
 
-if os.environ.get('GITHUB_WORKFLOW'):
+if os.getenv('CI'):
     TEST_SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@localhost:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 TEST_SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_SERVER}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}_test"
