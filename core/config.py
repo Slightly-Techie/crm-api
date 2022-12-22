44777680642a3789ddf346aad97921d80053a2b3
eeTiboah <etiboah@gmail.com>
2022-12-22 15:36:43 +0000

    add random string as secret for testing

--- core/config.py
+++ core/config.py
@@ -15,7 +15,7 @@ class Settings:
     POSTGRES_PORT : "str | int" = os.getenv("POSTGRES_PORT",5432) # default postgres port is 5432
     POSTGRES_DB : str = os.getenv("POSTGRES_DB","postgres")
     DATABASE_URL = os.getenv("DATABASE_URL",f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}")
-    SECRET: str = os.getenv("SECRET")
+    SECRET: str = os.getenv("SECRET", "ABnfjEINSKl3ECmsnoINEnwmkWAS")
     PRODUCTION_ENV : bool = os.getenv("PRODUCTION_ENV", False)
     ACCESS_TOKEN_EXPIRE_MINUTES: int = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60*24*30)
     ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
