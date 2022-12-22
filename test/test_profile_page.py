77bfba025a6fb7875ae74024e8a99d1aa2cc172a
JerryAgbesi <jerryeagbesi@gmail.com>
2022-12-22 12:35:54 +0000

    refactor tests

--- test/test_profile_page.py
+++ test/test_profile_page.py
@@ -1,6 +1,6 @@
 import json
 from fastapi.testclient import TestClient
-from api.api_models.user import Profile,UserResponse
+from api.api_models.user import ProfileResponse
 from app import app
 
 testclient = TestClient(app)
@@ -8,14 +8,15 @@ testclient = TestClient(app)
 def test_get_profile():
     res = testclient.get("/api/v1/users/profile/?id=1")
 
+    res_json = ProfileResponse(**res.json())
 
-    assert dict(res.json()[0])["email"] == "Korvo@gmail.com"
+    assert res_json.email == "Korvo@gmail.com"
     assert res.status_code == 200
 
 def test_update_profile():
     res = testclient.put("/api/v1/users/profile/?id=1",json={"github_profile":"https://github.com/Slightly-Techie/","twitter_profile":"https://twitter.com/slightlytechie","linkedin_profile":"https://linkedin.com/slightlytechie"})
     
-    res_json = Profile(**res.json())
+    res_json = ProfileResponse(**res.json())
 
     assert res_json.github_profile == "https://github.com/Slightly-Techie/"
     assert res.status_code == 200
