3eb1745d4e836eb80b5b65c3a1cd8fc3035e549a
Jonathan Ato Markin <jona10711@gmail.com>
2023-05-06 20:22:40 +0200

    Update user status

--- api/routes/profile_page.py
+++ api/routes/profile_page.py
@@ -1,7 +1,7 @@
 from typing import List
-from fastapi import APIRouter,Depends,HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-from api.api_models.user import ProfileUpdate,ProfileResponse
+from api.api_models.user import ProfileUpdate, ProfileResponse
 from core.config import settings
 from db.database import get_db
 from db.models.users import User
@@ -9,7 +9,8 @@ from utils.oauth2 import get_current_user
 from utils.permissions import is_admin
 
 
-profile_route = APIRouter(tags=["User"],prefix="/users")
+profile_route = APIRouter(tags=["User"], prefix="/users")
+
 
 @profile_route.get("/profile/{user_id}", response_model=ProfileResponse)
 async def get_profile(user_id: int, db: Session = Depends(get_db)):
@@ -17,28 +18,47 @@ async def get_profile(user_id: int, db: Session = Depends(get_db)):
     if user_details:
         return user_details
     else:
-        raise HTTPException(status_code=404, detail=settings.ERRORS.get("INVALID ID"))
+        raise HTTPException(
+            status_code=404, detail=settings.ERRORS.get("INVALID ID"))
 
-@profile_route.put("/profile",response_model=ProfileResponse)
-async def update_profile(userDetails:ProfileUpdate,current_user: User = Depends(get_current_user),db:Session = Depends(get_db)):
-    user_exists =  db.query(User).filter(User.id == current_user.id)
+
+@profile_route.put("/profile", response_model=ProfileResponse)
+async def update_profile(userDetails: ProfileUpdate, current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+    user_exists = db.query(User).filter(User.id == current_user.id)
     try:
         if user_exists.first():
-            update_data = userDetails.dict(exclude_unset=True) #convert json into a python dict and exclude fields not specified
+            # convert json into a python dict and exclude fields not specified
+            update_data = userDetails.dict(exclude_unset=True)
             user_exists.update(update_data)
-                        
+
             db.commit()
-       
-            return user_exists.first()         
+
+            return user_exists.first()
         else:
-                raise HTTPException(status_code=404,detail=settings.ERRORS.get("INVALID ID"))                       
+            raise HTTPException(
+                status_code=404, detail=settings.ERRORS.get("INVALID ID"))
     except:
-        raise HTTPException(status_code=400,detail=settings.ERRORS.get("UNKNOWN ERROR"))
-        
+        raise HTTPException(
+            status_code=400, detail=settings.ERRORS.get("UNKNOWN ERROR"))
+
+
 @profile_route.get("/", response_model=List[ProfileResponse])
-def get_all_profile(db:Session = Depends(get_db), current_user: User = Depends(is_admin)) -> List[ProfileResponse]:
-    
+def get_all_profile(db: Session = Depends(get_db), current_user: User = Depends(is_admin)) -> List[ProfileResponse]:
+
     user = db.query(User).all()
 
     return user
 
+
+@profile_route.put("/profile/{user_id}/activate", response_model=ProfileResponse, status_code=status.HTTP_200_OK)
+def update_profile_status(user_id: int, db: Session = Depends(get_db), current_user: User = Depends(is_admin)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(
+            status_code=404, detail=settings.ERRORS.get("INVALID ID"))
+    if user.is_active:
+        raise HTTPException(
+            status_code=400, detail=settings.ERRORS.get("USER ALREADY ACTIVE"))
+    user.is_active = True
+    db.commit()
+    return user
--- core/config.py
+++ core/config.py
@@ -5,26 +5,32 @@ from pathlib import Path
 env_path = Path('.') / '.env'
 load_dotenv()
 
+
 class Settings:
-    PROJECT_NAME:str = "Job Board"
+    PROJECT_NAME: str = "Job Board"
     PROJECT_VERSION: str = "1.0.0"
 
-    POSTGRES_USER : "str | None" = os.getenv("POSTGRES_USER", "postgres")
+    POSTGRES_USER: "str | None" = os.getenv("POSTGRES_USER", "postgres")
     POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
-    POSTGRES_SERVER : "str | None" = os.getenv("POSTGRES_SERVER","localhost")
-    POSTGRES_PORT : "str | int" = os.getenv("POSTGRES_PORT",5432) # default postgres port is 5432
-    POSTGRES_DB : str = os.getenv("POSTGRES_DB","postgres")
-    DATABASE_URL = os.getenv("DATABASE_URL",f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}")
+    POSTGRES_SERVER: "str | None" = os.getenv("POSTGRES_SERVER", "localhost")
+    POSTGRES_PORT: "str | int" = os.getenv(
+        "POSTGRES_PORT", 5432)  # default postgres port is 5432
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB", "postgres")
+    DATABASE_URL = os.getenv(
+        "DATABASE_URL", f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}")
     SECRET: str = os.getenv("SECRET", "ABnfjEINSKl3ECmsnoINEnwmkWAS")
-    PRODUCTION_ENV : bool = os.getenv("PRODUCTION_ENV", False)
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60*24*30)
+    PRODUCTION_ENV: bool = os.getenv("PRODUCTION_ENV", False)
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = os.getenv(
+        "ACCESS_TOKEN_EXPIRE_MINUTES", 60*24*30)
     ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
     ERRORS: dict = {
         "INVALID_CREDENTIALS": "Invalid Credentials",
         "PASSWORD_MATCH_DETAIL": "Passwords do not match",
         "USER_EXISTS": "User with email already exists",
-        "INVALID ID":"ID does not exist",
-        "UNKNOWN ERROR":"Something went wrong"
+        "INVALID ID": "ID does not exist",
+        "UNKNOWN ERROR": "Something went wrong",
+        "USER ALREADY ACTIVE": "User is already active",
     }
-    
+
+
 settings = Settings()
