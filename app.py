865379b401fb2beed8d15529f81232e984f5c828
CozyBrian <dragonbrian33@gmail.com>
2023-05-09 13:55:11 +0000

    allow_origin_regex fix

--- app.py
+++ app.py
@@ -20,15 +20,11 @@ origins = [
     "https://crm-web.fly.dev"
 ]
 
-regex_origin = [
-    "https:\/\/.*\.uffizzi\.com"
-]
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_origin_regex=regex_origin,
+    allow_origin_regex="https:\/\/.*\.uffizzi\.com",
     allow_methods=["GET", "POST", "PUT", "DELETE", "UPDATE", "OPTIONS"],
     allow_headers=["*"],
 )
