c0dee296c9c398bf53b3b5bef4ce6ff018ff3a9c
greatnessmensah <ephraimmensah99@gmail.com>
2023-05-12 09:59:02 +0000

    wrote test for getting a single post

--- test/test_feeds.py
+++ test/test_feeds.py
@@ -1,3 +1,6 @@
+from api.api_models.user import Feeds
+
+
 def test_get_all_feeds(client, test_feeds):
     response = client.get("/api/v1/feed/")
     assert response.status_code == 200
@@ -19,3 +22,18 @@ def test_get_all_feeds_pagination(client, test_feeds, session):
     assert feeds["feeds"][0]["title"] == test_feeds[::-1][0].title
     assert feeds["feeds"][1]["content"] == test_feeds[::-1][1].content
     assert feeds["feeds"][1]["user"]["id"] == test_feeds[::-1][1].user.id
+
+
+def test_get_one_feed(client, test_feeds):
+    res = client.get(f"/api/v1/feed/{test_feeds[0].id}")
+    feed = Feeds(**res.json())
+    assert feed.id == test_feeds[0].id
+    assert feed.title == test_feeds[0].title
+    assert feed.content == test_feeds[0].content
+    assert feed.user.id == test_feeds[0].user.id
+    assert res.status_code == 200
+
+
+def test_get_one_post_does_not_exist(client, test_feeds):
+    res = client.get(f"api/v1/feed/10000")
+    assert res.status_code == 404
