2bd170f5ae38184019ebb4e9cf2ee33025d3d3c0
dvc-77 <neilohene@gmail.com>
2023-08-13 20:37:11 +0000

    update: replaced smtplib with yagmail for sending mails

--- utils/mail_service.py
+++ utils/mail_service.py
@@ -1,44 +1,28 @@
 from typing import Tuple
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-
-from typing import Tuple
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
+import yagmail
 from urllib.parse import urlencode
 
 def send_reset_password_email(email: str, reset_token: str) -> Tuple[str, str]:
-    sender_email = "user@example.com"
-    sender_password = "your_password" 
+    sender_email = "user@example.com" # Set up env variables for your mail, and app password
+    sender_password = "app_password" 
 
-    reset_password_url = f"http://127.0.0.1:8000/reset-password?{urlencode({'token': reset_token})}"
+    reset_password_url = f"http://127.0.0.1:8080/reset-password?{urlencode({'token': reset_token})}"
 
-    message = MIMEMultipart("alternative")
-    message["Subject"] = "Reset Password"
-    message["From"] = sender_email
-    message["To"] = email
-
+    subject = "Reset Password"
     text = f"Click on this link to reset your password: {reset_password_url}"
     html = f"""\
-                    <html>
-                        <body>
-                            <p>Click on this link to reset your password:</p>
-                            <p><a href="{reset_password_url}">{reset_password_url}</a></p>
-                        </body>
-                    </html>
+                <html>
+                    <body>
+                        <p>Click on this link to reset your password:</p>
+                        <p><a href="{reset_password_url}">{reset_password_url}</a></p>
+                    </body>
+                </html>
             """
 
-    part1 = MIMEText(text, "plain")
-    part2 = MIMEText(html, "html")
-
-    message.attach(part1)
-    message.attach(part2)
-
-    with smtplib.SMTP("smtp.gmail.com", 587) as server:
-        server.starttls()
-        server.login(sender_email, sender_password)
-        server.sendmail(sender_email, email, message.as_string())
-
+    yag = yagmail.SMTP(sender_email, sender_password)
+    
+    contents = [text, html]
+    
+    yag.send(to=email, subject=subject, contents=contents)
+    
     return "Reset password email sent successfully", email
