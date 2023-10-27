e028c1efa4e2db63a0d5ed1ff7a23d5c7a6630d5
greatnessmensah <ephraimmensah99@gmail.com>
2023-10-27 12:50:49 +0000

    solved merge conflict

--- test/test_auth.py
+++ test/test_auth.py
@@ -1,14 +1,4 @@
 from datetime import datetime, timedelta
-
-import pytest
-from fastapi import HTTPException
-from fastapi.testclient import TestClient
-from jose import jwt
-from api.api_models.user import ForgotPasswordRequest, ResetPasswordRequest
-from api.routes.auth import reset_password
-from app import app
-from datetime import datetime, timedelta
-
 import pytest
 from fastapi import HTTPException
 from fastapi.testclient import TestClient
@@ -20,10 +10,6 @@ from core.config import settings
 from utils.mail_service import send_email
 from utils.oauth2 import create_reset_token, verify_reset_token
 
-client = TestClient(app)
-from core.config import settings
-from utils.mail_service import send_email
-from utils.oauth2 import create_reset_token, verify_reset_token
 
 client = TestClient(app)
 
@@ -57,11 +43,9 @@ def test_user_signup_valid(client):
     assert res.status_code == 201
     
 def test_user_signup_invalid():
-def test_user_signup_invalid():
     res = client.post("/api/v1/users/register", json=user_signup_payload_incomplete)
     assert res.status_code == 422
 
-def test_user_signup_invalid_confirm_password():
 def test_user_signup_invalid_confirm_password():
     payload = user_signup_payload.copy()
     payload["password_confirmation"] = "Test1235"
@@ -71,7 +55,6 @@ def test_user_signup_invalid_confirm_password():
     assert res.status_code == 400
     assert res_body["detail"] == settings.ERRORS.get("PASSWORD_MATCH_DETAIL")
     
-def test_user_login_valid(test_user):
 def test_user_login_valid(test_user):
     res = client.post("/api/v1/users/login", data={
         "username": test_user.get("email"),
@@ -83,7 +66,6 @@ def test_user_login_valid(test_user):
     assert res_body["token"] is not None
     
 def test_user_login_invalid(test_user):
-def test_user_login_invalid(test_user):
     res = client.post("/api/v1/users/login", data={
         "username": test_user.get("email"),
         "password": "Test1235"
@@ -93,7 +75,6 @@ def test_user_login_invalid(test_user):
     assert res.status_code == 400
     assert res_body["detail"] == settings.ERRORS.get("INVALID_CREDENTIALS")
 
-def test_user_gets_token_and_refresh_token(test_user):
 def test_user_gets_token_and_refresh_token(test_user):
     login_response = client.post(
         "/api/v1/users/login",
@@ -111,52 +92,6 @@ def test_user_gets_token_and_refresh_token(test_user):
 
     assert "token" in refresh_token_response
     assert "refresh_token" in refresh_token_response
-
-
-def test_create_reset_token_valid_email(test_user, mocker):
-    email = test_user.get("email")
-    expected_token = 'valid_token'
-    mocker.patch('jose.jwt.encode', return_value=expected_token)
-    assert create_reset_token(email) == expected_token
-
-def test_verify_reset_token_invalid_email_format():
-    invalid_email = 'invalid_email'
-    token = create_reset_token(invalid_email)
-    with pytest.raises(HTTPException) as exc:
-        verify_reset_token(token)
-    assert exc.value.status_code == 400
-    assert exc.value.detail == 'Invalid token'
-
-def test_verify_reset_token_expired_token():
-    email = 'test@example.com'
-    delta = timedelta(minutes=-15)
-    now = datetime.utcnow()
-    payload = {
-        'sub': email,
-        'iat': now,
-        'exp': now + delta
-    }
-    expired_token = jwt.encode(payload, settings.SECRET, algorithm=settings.ALGORITHM)
-    with pytest.raises(HTTPException) as exc:
-        verify_reset_token(expired_token)
-    assert exc.value.status_code == 400
-
-def test_reset_password_invalid_token():
-    with pytest.raises(HTTPException) as e:
-        reset_password(ResetPasswordRequest(token='invalid_token', new_password='new_password'))
-    assert e.value.status_code == 400
-    assert e.value.detail == 'Invalid token'
-
-#test needs a valid email.
-
-# @pytest.mark.asyncio
-# async def test_send_email():
-#     email = "test@example.com"
-#     reset_token = "test_reset_token"
-#     response = await send_email(email, reset_token)
-#     assert response.status_code == 200
-    
-
 
 
 def test_create_reset_token_valid_email(test_user, mocker):
