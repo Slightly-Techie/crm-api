bbd096bf3484b3324aaa03172996ac731829dce8
TMCreme <tonnybright123@yahoo.com>
2024-02-19 16:37:07 +0000

    linting fix for flake8

--- core/exceptions.py
+++ core/exceptions.py
@@ -2,18 +2,19 @@ from fastapi import HTTPException, status
 
 
 class ValidationError(Exception):
-  def __init__(self, error_msg: str, status_code: int):
-    super().__init__(error_msg)
-    self.status_code = status_code
-    self.error_msg = error_msg
+    def __init__(self, error_msg: str, status_code: int):
+        super().__init__(error_msg)
+        self.status_code = status_code
+        self.error_msg = error_msg
+
 
 class UserNotFoundError(ValidationError):
-  pass
-
+    pass
 
 
 class ForbiddenError(HTTPException):
-  def __init__(self, status_code: int = status.HTTP_403_FORBIDDEN, 
-      detail:str = "User not authorised"):
-    super().__init__(status_code, detail)
- 
+    def __init__(
+        self, status_code: int = status.HTTP_403_FORBIDDEN,
+        detail: str = "User not authorised"
+    ):
+        super().__init__(status_code, detail)
