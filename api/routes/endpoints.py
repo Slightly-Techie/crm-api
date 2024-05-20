7cff3953a4d557426c61238786d5417d46683e5c
greatnessmensah <ephraimmensah99@gmail.com>
2024-05-20 14:38:17 +0000

    added authorization to endpoint status api

--- api/routes/endpoints.py
+++ api/routes/endpoints.py
@@ -3,13 +3,15 @@ from fastapi import APIRouter, Depends, HTTPException, status, Body
 from sqlalchemy.orm import Session
 from db.database import get_db
 from db.models.endpoints import Endpoints
+from db.models.users import User
+from utils.permissions import is_admin
 
 
 endpoints_route = APIRouter(prefix="/endpoints", tags=["Endpoints"])
 
 
 @endpoints_route.post("/")
-async def add_endpoint_status(endpoint: str = Body(...), db: Session = Depends(get_db)):
+async def add_endpoint_status(endpoint: str = Body(...), db: Session = Depends(get_db), user: User = Depends(is_admin)):
     endpoint_query = db.query(Endpoints).filter(Endpoints.endpoint == endpoint).first()
     if endpoint_query:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Endpoint already exists")
@@ -20,7 +22,7 @@ async def add_endpoint_status(endpoint: str = Body(...), db: Session = Depends(g
 
 
 @endpoints_route.put("/")
-async def toggle_endpoint(endpoint: str = Body(...), db: Session = Depends(get_db)):
+async def toggle_endpoint(endpoint: str = Body(...), db: Session = Depends(get_db), user: User = Depends(is_admin)):
     endpoint_query = db.query(Endpoints).filter(Endpoints.endpoint == endpoint).first()
     if not endpoint_query:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Endpoint not found")
