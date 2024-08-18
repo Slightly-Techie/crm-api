d4084ce060c72b8e566c701d3a7c19b77b44e2b9
TMCreme <tonnybright123@yahoo.com>
2024-08-18 22:24:02 +0000

    tech task mail service fix

--- utils/mail_service.py
+++ utils/mail_service.py
@@ -41,6 +41,7 @@ async def send_email(subject: str, recipient_email: str, html_content: str) -> J
     except Exception as e:
         return JSONResponse(status_code=500, content={"message": f"An error occurred: {e}"})
 
+
 async def send_password_reset_email(email: str, reset_token: str, username: str,db: Session = Depends(get_db)):
     subject = "Reset Password"
     reset_password_url = f"{settings.BASE_URL}{settings.URL_PATH}?{urlencode({'token': reset_token})}"
@@ -57,24 +58,32 @@ async def send_password_reset_email(email: str, reset_token: str, username: str,
     
     await send_email(email_template.subject, email, html_content)
 
+
 async def send_applicant_task(
         email: str, first_name: str, task: str):
     """Send technical task to the applicant"""
-    email_sender = settings.EMAIL_SENDER
-    email_password = settings.EMAIL_PASSWORD
-    email_receiver = f'{email}'
+    # email_sender = settings.EMAIL_SENDER
+    # email_password = settings.EMAIL_PASSWORD
+    # email_receiver = f'{email}'
     subject = "Invitation to Technical Challenge"
-    html = task.format(first_name)
+    try:
+        html_content = read_html_file('utils/email_templates/task_template.html').format(first_name, task)
+    except FileNotFoundError:
+        print("File not found")
+        html_content = f"Hello {first_name}, Kindly work on the this task and submit:\n {task}"
+    # html = task.format(first_name)
 
-    em = MIMEMultipart()
-    em['From'] = email_sender
-    em['To'] = email_receiver
-    em['Subject'] = subject
-    html_part = MIMEText(html, 'html')
-    em.attach(html_part)
+    # em = MIMEMultipart()
+    # em['From'] = email_sender
+    # em['To'] = email_receiver
+    # em['Subject'] = subject
+    # html_part = MIMEText(html, 'html')
+    # em.attach(html_part)
 
-    context = ssl.create_default_context()
+    # context = ssl.create_default_context()
 
-    with smtplib.SMTP_SSL(settings.EMAIL_SERVER, settings.EMAIL_PORT, context=context) as smtp:
-        smtp.login(email_sender, email_password)
-        smtp.sendmail(email_sender, email_receiver, em.as_string())
+    await send_email(subject, email, html_content)
+
+    # with smtplib.SMTP_SSL(settings.EMAIL_SERVER, settings.EMAIL_PORT, context=context) as smtp:
+    #     smtp.login(email_sender, email_password)
+    #     smtp.sendmail(email_sender, email_receiver, em.as_string())
